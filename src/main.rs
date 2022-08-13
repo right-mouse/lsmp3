c8c2ac651f1076cc87c3ef0f4e23a0e295dc04f7
Arvind Suresh <arvind.suresh60@gmail.com>
2022-08-12 21:00:57 -0400

    Update dependencies

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@
 //! Works similar to `ls`, but ignores all files that are not MP3s with valid ID3 tags. Various options are provided for
 //! sorting. In addition to a human readable table format, JSON output is also supported.
 
-use clap::{clap_derive::ArgEnum, CommandFactory, Parser};
+use clap::{clap_derive::ArgEnum, CommandFactory, Parser, ValueHint};
 use lsmp3::ls;
 use serde_json::{json, Value};
 use std::{error::Error, io::Write};
@@ -34,16 +34,14 @@ enum Format {
 #[clap(version, about, long_about = None)]
 struct Args {
     /// The FILEs to list information about (the current directory by default)
-    #[clap(multiple = true)]
-    #[clap(value_parser)]
+    #[clap(value_hint = ValueHint::AnyPath)]
     file: Vec<String>,
 
-    /// Output format to use
+    /// The output format to use
     #[clap(long = "format", short = 'f')]
     #[clap(value_name = "WORD")]
     #[clap(arg_enum)]
     #[clap(default_value = "table")]
-    #[clap(value_parser)]
     format: Format,
 
     /// Reverse order while sorting
@@ -61,7 +59,6 @@ struct Args {
     #[clap(multiple = true)]
     #[clap(number_of_values = 1)]
     #[clap(default_value = "file-name")]
-    #[clap(value_parser)]
     sort_by: Vec<ls::SortBy>,
 }
 
@@ -74,12 +71,78 @@ fn to_table(res: &[ls::Entry]) -> String {
             .with(tabled::Style::blank())
             .with(tabled::Modify::new(tabled::object::Segment::all()).with(tabled::Alignment::left()))
             .to_string()
+            + "\n"
     }
 }
 
 #[inline]
 fn to_json(res: &[ls::Entry]) -> Value {
     serde_json::to_value(res).unwrap_or_else(|err| error(err))
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let results = ls::list(
+        &args.file,
+        &ls::ListOptions {
+            sort_by: &args.sort_by,
+            reverse: &args.reverse,
+            recursive: &args.recursive,
+        },
+    )
+    .unwrap_or_else(|err| error(err));
+    let (mut tables, mut values): (Vec<String>, Vec<Value>) = match args.format {
+        Format::Table => (Vec::with_capacity(results.len()), Vec::with_capacity(0)),
+        Format::JSON => (Vec::with_capacity(0), Vec::with_capacity(results.len())),
+    };
+    if results.len() == 1 {
+        match args.format {
+            Format::Table => tables.push(to_table(&results[0].entries)),
+            Format::JSON => values.push(to_json(&results[0].entries)),
+        }
+    } else {
+        let (files, dirs): (Vec<_>, Vec<_>) = results.into_iter().partition(|f| f.path_type == ls::PathType::File);
+        if !files.is_empty() {
+            let mut f = files.into_iter().map(|f| f.entries).flatten().collect::<Vec<_>>();
+            f.sort_unstable_by(|a, b| {
+                let ord = ls::cmp_entry(a, b, &args.sort_by);
+                if args.reverse {
+                    ord.reverse()
+                } else {
+                    ord
+                }
+            });
+            match args.format {
+                Format::Table => tables.push(to_table(&f)),
+                Format::JSON => values.push(to_json(&f)),
+            }
+        }
+        if !dirs.is_empty() {
+            match args.format {
+                Format::Table => tables.extend(dirs.iter().map(|f| format!("{}:\n{}", f.path, to_table(&f.entries)))),
+                Format::JSON => values.extend(dirs.iter().map(|f| {
+                    json!({
+                        "path": f.path,
+                        "values": to_json(&f.entries),
+                    })
+                })),
+            }
+        }
+    }
+
+    print!(
+        "{}",
+        match args.format {
+            Format::Table => tables.join("\n"),
+            Format::JSON => if values.len() == 1 {
+                serde_json::to_string(&values[0])
+            } else {
+                serde_json::to_string(&values)
+            }
+            .unwrap_or_else(|err| error(err)),
+        }
+    )
 }
 
 #[cfg(test)]
@@ -181,69 +244,9 @@ mod tests {
             ])
         )
     }
-}
 
-fn main() {
-    let args = Args::parse();
-
-    let results = ls::list(
-        &args.file,
-        &ls::ListOptions {
-            sort_by: &args.sort_by,
-            reverse: &args.reverse,
-            recursive: &args.recursive,
-        },
-    )
-    .unwrap_or_else(|err| error(err));
-    let (mut tables, mut values): (Vec<String>, Vec<Value>) = match args.format {
-        Format::Table => (Vec::with_capacity(results.len()), Vec::with_capacity(0)),
-        Format::JSON => (Vec::with_capacity(0), Vec::with_capacity(results.len())),
-    };
-    if results.len() == 1 {
-        match args.format {
-            Format::Table => tables.push(to_table(&results[0].entries)),
-            Format::JSON => values.push(to_json(&results[0].entries)),
-        }
-    } else {
-        let (files, dirs): (Vec<_>, Vec<_>) = results.into_iter().partition(|f| f.path_type == ls::PathType::File);
-        if !files.is_empty() {
-            let mut f = files.into_iter().map(|f| f.entries).flatten().collect::<Vec<_>>();
-            f.sort_unstable_by(|a, b| {
-                let ord = ls::cmp_entry(a, b, &args.sort_by);
-                if args.reverse {
-                    ord.reverse()
-                } else {
-                    ord
-                }
-            });
-            match args.format {
-                Format::Table => tables.push(to_table(&f)),
-                Format::JSON => values.push(to_json(&f)),
-            }
-        }
-        if !dirs.is_empty() {
-            match args.format {
-                Format::Table => tables.extend(dirs.iter().map(|f| format!("{}:\n{}", f.path, to_table(&f.entries)))),
-                Format::JSON => values.extend(dirs.iter().map(|f| {
-                    json!({
-                        "path": f.path,
-                        "values": to_json(&f.entries),
-                    })
-                })),
-            }
-        }
+    #[test]
+    fn verify_args() {
+        Args::command().debug_assert()
     }
-
-    print!(
-        "{}",
-        match args.format {
-            Format::Table => tables.join("\n"),
-            Format::JSON => if values.len() == 1 {
-                serde_json::to_string(&values[0])
-            } else {
-                serde_json::to_string(&values)
-            }
-            .unwrap_or_else(|err| error(err)),
-        }
-    )
 }
